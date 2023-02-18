cd19c77334ae94911ce13d6f85b223d793634d04
Jaciubek <slawomir.jaciubek@gmail.com>
2023-02-18 19:39:37 +0100

    fix base url mockapi.io

--- src/index.js
+++ src/index.js
@@ -15,9 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <Routes>
-            <Route path="/*" element={<App />} />
-          </Routes>
+          <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
