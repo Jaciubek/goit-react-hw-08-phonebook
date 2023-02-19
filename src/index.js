d579cb6c8273a54d995679742d02448902e6d6c9
Jaciubek <slawomir.jaciubek@gmail.com>
2023-02-19 17:47:34 +0100

    first commit2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store, persistor } from 'redux/store';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
